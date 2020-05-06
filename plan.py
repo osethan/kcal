312af6e6ab44438100f85098e59fd14ef61ea279
osethan <GE47R9xmtouch .zshrcFASEPs@0>
2020-05-06 09:59:07 -0700

    Debugged finds fitted measurements for kcal and cup for ingredients

--- plan.py
+++ plan.py
@@ -1,33 +1,63 @@
 
-def mealPlans(ingredients, meals, factors = [], start = 0, end = 2, step = 1/8, max_kcal = 900, min_kcal = 850, max_cup = 3, min_cup = 2):
+def mealPlans(ingredients, meals, factors = [0]):
   """
   Make meal plans.
   """
 
+  # print('factors', factors)
+  servings = {
+    'start': 0,
+    'end': 1,
+    'step': 0.25,
+    'kcal': {
+      'max': 900,
+      'min': 850
+    },
+    'cup': {
+      'max': 3,
+      'min': 2
+    }
+  }
+
   # Recursion base cases
-  if factors[-1] > max_cup:
+  if factors[-1] > servings['end']:
     return []
   if len(factors) > len(ingredients):
     return []
 
+  # print('factors', factors)
+  factors_left = [f for f in factors]
+  factors_left[-1] += servings['step']
+  factors_right = [f for f in factors]
+  factors_right += [servings['start']]
+  meal_plans = mealPlans(ingredients, meals, factors_left) + mealPlans(ingredients, meals, factors_right)
   if len(factors) < len(ingredients):
-    factors_left = [f for f in factors]
-    factors_left[-1] += step
-    factors_right = [f for f in factors]
-    factors_right += [start]
-    return mealPlans(ingredients, meals, factors_left, start, end, step, max_kcal, min_kcal, max_cup, min_cup) + mealPlans(ingredients, meals, factors_right, start, end, step, max_kcal, min_kcal, max_cup, min_cup)
+    return meal_plans
   else:
-    if fitsPlan(ingredients, factors, max_kcal, min_kcal, max_cup, min_cup):
-      return factors
+    if fitsPlan(ingredients, factors):
+      return factors + meal_plans
     else:
-      return []
+      return meal_plans
 
 
-def fitsPlan(ingredients, factors, max_kcal, min_kcal, max_cup, min_cup):
+def fitsPlan(ingredients, factors):
   """
   Fit recipe.
   """
 
+  servings = {
+    'start': 0,
+    'end': 1,
+    'step': 1/4,
+    'kcal': {
+      'max': 900,
+      'min': 850
+    },
+    'cup': {
+      'max': 3,
+      'min': 2
+    }
+  }
   kcal_avgs = {
     'fruit': 100,
     'grain': 200,
@@ -37,19 +67,19 @@ def fitsPlan(ingredients, factors, max_kcal, min_kcal, max_cup, min_cup):
   kcals = 0
   cups = 0
   for i in range(len(ingredients)):
-    kcals += kcal_avgs[ingredients[i]['list']] + factors[i]
+    kcals += kcal_avgs[ingredients[i]['list']] * factors[i]
     cups += factors[i]
 
-  return min_kcal <= kcals <= max_kcal and min_cup <= cups <= max_cup
+  return servings['kcal']['min'] <= kcals <= servings['kcal']['max'] and servings['cup']['min'] <= cups <= servings['cup']['max']
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
   ingredients = [{
-    'title': 'oatmeal',
-    'list': 'grain'
-  }, {
     'title': 'fruit',
     'list': 'fruit'
+  }, {
+    'title': 'oatmeal',
+    'list': 'grain'
   }, {
     'title': 'peanuts',
     'list': 'nuts'
@@ -57,4 +87,6 @@ if __name__ == "__main__":
   meals = 1
 
   meal_plans = mealPlans(ingredients, meals)
-  print(meal_plans)
+  size = len(ingredients)
+  meal_plans = [meal_plans[i:i + size] for i in range(0, len(meal_plans), size)]
+  print('meal_plans', meal_plans)
