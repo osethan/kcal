84ee4e4f3e8ab7860959c56c6d35e552a5dd6f45
osethan <GE47R9xmtouch .zshrcFASEPs@0>
2020-05-07 09:52:48 -0700

    Expand ingredients data structure for more meals

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # kcal
+
+30% calories should come from fat (https://k-p.li/2zjb9Hq)
--- plan.py
+++ plan.py
@@ -1,17 +1,20 @@
 
-def mealPlans(ingredients, meals, factors = [0]):
+def mealPlans(ingredients, recipe = []):
   """
   Make meal plans.
   """
 
-  # print('factors', factors)
   servings = {
     'start': 0,
-    'end': 1,
+    'end': 3,
     'step': 0.25,
-    'kcal': {
-      'max': 900,
-      'min': 850
+    # 'kcal': {
+    #   'max': 900,
+    #   'min': 850
+    # },
+    'kcal': { # 70% rice, 30% dressing
+      'max': 630,
+      'min': 595
     },
     'cup': {
       'max': 3,
@@ -19,74 +22,97 @@ def mealPlans(ingredients, meals, factors = [0]):
     }
   }
 
+  # Mutate ingredients
+  if len(recipe) == 0:
+    recipe += [ingredients[0]]
+  if 'cups' not in recipe[-1]:
+    recipe[-1]['cups'] = servings['start']
+
   # Recursion base cases
-  if factors[-1] > servings['end']:
-    return []
-  if len(factors) > len(ingredients):
+  if recipe[-1]['cups'] > servings['end']:
     return []
 
-  # print('factors', factors)
-  factors_left = [f for f in factors]
-  factors_left[-1] += servings['step']
-  factors_right = [f for f in factors]
-  factors_right += [servings['start']]
-  meal_plans = mealPlans(ingredients, meals, factors_left) + mealPlans(ingredients, meals, factors_right)
-  if len(factors) < len(ingredients):
-    return meal_plans
+  recipe_left = [r for r in recipe]
+  recipe_left[-1]['cups'] += servings['step']
+  if len(recipe) < len(ingredients):
+    recipe_right = [r for r in recipe]
+    recipe_right += [ingredients[len(recipe_right)]]
+    return mealPlans(ingredients, recipe_left) + mealPlans(ingredients, recipe_right)
   else:
-    if fitsPlan(ingredients, factors):
-      return factors + meal_plans
+    if fitsPlan(recipe):
+      return recipe + mealPlans(ingredients, recipe_left)
     else:
-      return meal_plans
+      return mealPlans(ingredients, recipe_left)
 
 
-def fitsPlan(ingredients, factors):
+# def fitsPlan(ingredients, factors):
+def fitsPlan(recipe):
   """
   Fit recipe.
   """
 
   servings = {
     'start': 0,
-    'end': 1,
-    'step': 1/4,
-    'kcal': {
-      'max': 900,
-      'min': 850
+    'end': 3,
+    'step': 0.25,
+    # 'kcal': {
+    #   'max': 900,
+    #   'min': 850
+    # },
+    'kcal': { # 70% rice, 30% dressing
+      'max': 630,
+      'min': 595
     },
     'cup': {
       'max': 3,
       'min': 2
     }
   }
-  kcal_avgs = {
+  kcal_avgs = { # kcal / cup
+    'beans': 200,
     'fruit': 100,
     'grain': 200,
-    'nuts': 800
+    'nuts': 800,
+    'oil': 1600,
+    'spices': 0,
+    'vegetables': 100
   }
 
-  kcals = 0
-  cups = 0
-  for i in range(len(ingredients)):
-    kcals += kcal_avgs[ingredients[i]['list']] * factors[i]
-    cups += factors[i]
+  kcal = 0
+  cup = 0
+  for ingredient in recipe:
+    kcal += kcal_avgs[ingredient['type']] * ingredient['cups']
+    cup += ingredient['cups']
 
-  return servings['kcal']['min'] <= kcals <= servings['kcal']['max'] and servings['cup']['min'] <= cups <= servings['cup']['max']
+  return servings['kcal']['min'] <= kcal <= servings['kcal']['max'] and servings['cup']['min'] <= cup <= servings['cup']['max']
 
 
 if __name__ == '__main__':
-  ingredients = [{
-    'title': 'fruit',
-    'list': 'fruit'
+  # ingredients = [{ # Breakfast
+  #   'title': 'fruit',
+  #   'type': 'fruit'
+  # }, {
+  #   'title': 'oatmeal',
+  #   'type': 'grain'
+  # }, {
+  #   'title': 'peanuts',
+  #   'type': 'nuts'
+  # }]
+  ingredients = [{ # Lunch
+    'title': 'brown rice',
+    'type': 'grain'
   }, {
-    'title': 'oatmeal',
-    'list': 'grain'
+    'title': 'vegetables',
+    'type': 'vegetables'
   }, {
-    'title': 'peanuts',
-    'list': 'nuts'
+    'title': 'olive oil',
+    'type': 'oil'
+  }, {
+    'title': 'beans',
+    'type': 'beans'
   }]
-  meals = 1
 
-  meal_plans = mealPlans(ingredients, meals)
+  meal_plans = mealPlans(ingredients)
   size = len(ingredients)
   meal_plans = [meal_plans[i:i + size] for i in range(0, len(meal_plans), size)]
   print('meal_plans', meal_plans)
